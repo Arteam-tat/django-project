99c1a61ad56cd90abf2f5adbc636a204860ac207
Arteam-tat <arteamtatarinovmeow@gmail.com>
2023-05-29 14:16:18 +0300

    initial commit

---
+++ 1.1-first-project/first_project/app/views.py
@@ -0,0 +1,32 @@
+import os
+import datetime
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    
+
+
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    current_path = os.listdir('.')
+    return HttpResponse(f'Список файлов рабочей директории {current_path}')
